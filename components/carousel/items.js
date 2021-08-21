f8e97cea2c0fe104b858301685f639a2c6e964d6
maxo <maximino33@hotmail.com>
2021-08-20 19:14:17 -0500

    rutas de canciones actualizadas

--- components/carousel/items.js
+++ components/carousel/items.js
@@ -3,13 +3,13 @@ export const contenidoItems = [
         src: 'https://www.eltiempo.com/files/image_640_428/uploads/2019/10/05/5d9927fc77c15.jpeg',
         name: 'Tal vez',
         autor: 'Paulo Londra',
-        song: ''
+        song: 'talVez.mp3'
     },
     {
         src: 'https://rtvc-assets-radionica3.s3.amazonaws.com/s3fs-public/styles/image_750x424/public/field/image/article/canserbero_02.jpg?itok=2X525z7X',
         name: 'Jeremías 17-5',
         autor: 'Cancerbero',
-        song: 'a-little-more-time.mp3'
+        song: 'EsEpico.mp3'
     },
     {
         src: 'https://indietimemx.files.wordpress.com/2020/09/mac-miller-e1533930950967.jpg?w=1024',
@@ -21,7 +21,7 @@ export const contenidoItems = [
         src: 'https://rolandoradio.files.wordpress.com/2019/09/95b6567d9338d90f9123b9b4085da9e6.jpg',
         name: 'Maldito romantico',
         autor: 'Gera MX',
-        song: 'a-little-more-time.mp3'
+        song: 'malditoRomantico.mp3'
     },
     {
         src: 'https://direct.rhapsody.com/imageserver/images/Art.26018277/356x237.jpg',
@@ -39,24 +39,24 @@ export const contenidoItems = [
         src: 'https://wololosound.com/wp-content/uploads/david-guetta-deejay-concert.jpg',
         name: 'Memories',
         autor: 'David Guetta',
-        song: 'a-little-more-time.mp3'
+        song: 'memories.mp3'
     },
     {
-        src: 'https://www.alasarmas.org/wp-content/uploads/2017/02/Bob-Dylan.jpg',
-        name: 'Like a rolling stone',
-        autor: 'Bob dylan',
-        song: 'a-little-more-time.mp3'
+        src: 'https://i1.sndcdn.com/artworks-000047020702-b8cey9-t500x500.jpg',
+        name: 'Dancing',
+        autor: 'Aaron Smith',
+        song: 'dancing.mp3'
     },
     {
         src: 'https://ichef.bbci.co.uk/news/640/cpsprodpb/149B8/production/_107480448_d1d6d7b2-24c5-464a-93f0-836d5c0da9c3.jpg',
         name: 'Dont stop me now',
         autor: 'Queen',
-        song: 'a-little-more-time.mp3'
+        song: 'dontStopMeNow.mp3'
     },
     {
-        src: 'https://www.eltiempo.com/files/image_640_428/uploads/2019/10/05/5d9927fc77c15.jpeg',
-        name: 'Tal vez',
-        autor: 'Paulo Londra',
-        song: 'a-little-more-time.mp3'
+        src: 'https://cdn.smehost.net/sonymusices-45pressprod/wp-content/uploads/2020/06/BEP-2-by-Nabil-Elderkin-scaled.jpg',
+        name: 'The Time',
+        autor: 'The Black Eyed Peas',
+        song: 'theTime.mp3'
     },
 ]
